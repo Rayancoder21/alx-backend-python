b1e36652a8ed83955c985c4c4eefca8bcd5d8a2a
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-08-09 13:18:51 +0000

    fix some errors

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""create Tasks"""
+"""Create a task"""
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task
-"""returns a asyncio.Task"""
- return asyncio.create_task(wait_random(max_delay))
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return a task"""
+    return asyncio.create_task(wait_random(max_delay))
